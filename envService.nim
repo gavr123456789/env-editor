5f2d1a16264d649215856056a167e2190640a7f5
gavr <gavr123456789@gmail.com>
2021-10-14 02:45:50 +0300

    make funcs public

--- envService.nim
+++ envService.nim
@@ -4,12 +4,12 @@ import unpack, print
 const PATH = "PATH"
 
 type 
-  EnvVar = tuple
+  EnvVar* = tuple
     line: int
     value: string
     key: string
     isPath: bool
-  EnvVars = seq[EnvVar]
+  EnvVars* = seq[EnvVar]
 
 proc toString(env: EnvVars): string =
   for x in env:
@@ -23,27 +23,26 @@ proc toString(env: EnvVars): string =
 
 proc getFishPath: string = getHomeDir() / ".config" / "fish" / "config.fish"
 
-proc getFishEnv(): EnvVars =
+proc getFishEnv*(): EnvVars =
   var fileContentLines = getFishPath().readFile.split "\n"
   for i, line in fileContentLines:
     if line == "": break
     [_, k, v] <- line.split " "
     result.add (line: i, value: v, key: k, isPath: k==PATH)
 
-proc addKV(env: var EnvVars, k, v: string) =
+proc addKV*(env: var EnvVars, k, v: string) =
   env.add (line: env.high + 1, value: v, key: k, isPath: k==PATH)
   
-proc writeFishEnv(env: EnvVars) =
+proc writeFishEnv*(env: EnvVars) =
   let fishConfigFile = getFishPath()
   writeFile(fishConfigFile, env.toString)
 
 when isMainModule:
   var x = getFishEnv()
+  # x.addKV("Sas", "sas")
+  x.addKV("PATH", "/home/gavr/bin")
   print x
-  # x.addKV("Sas", "sud")
-  # x.addKV("PATH", "sud")
-  # print x
-  # writeFishEnv x
+  writeFishEnv x
   
   # for a in envPairs():
   #   print a
